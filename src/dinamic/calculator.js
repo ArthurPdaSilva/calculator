3116283fc75077c2e342fa1db3e46349198fe2a5
João Victor Lacerda de Queiroz <victorqueiroz90@outlook.com>
2021-05-14 17:31:50 -0300

    opa, meu fiii. Espero que dê bom

--- src/dinamic/calculator.js
+++ src/dinamic/calculator.js
@@ -1,45 +1,25 @@
 // Variable
 let field = document.querySelector("input.field");
-let testValue = [];
+let testValue = ['-','+','*','/','.',')'];
 
 // Calculator System
 function calc(x){
-    testValue = [];
     field.value += x;
 }
 
 // Verified of the value
 // testValue.push('.', '*', '/', '(', ')', '-', '+');
-
 function operator(y = 0){
-    console.log(testValue[testValue.length -1]);
-    if(field.value.length == 0){
-        if(y == '*' || y == '/' || y == '.' || y == ')'){
-            return false;
-        }else{
-            testValue.push('-', '+', '(');
-            field.value += y;
-        }
-    }else{
-        if(testValue.includes(y)){
-            return false;
-        }else{
-            field.value += y;
-            if(y == '*' || y == '/'){
-                testValue.push('*', '/', '.');
-            }else if(y == '+' || y == '-'){
-                testValue.push('-', '+', '.');
-            }else if(y == ')' || y == '('){
-                testValue.push('(', ')', '.');
-            }
-            // testValue.push('.', '*', '/', '-', '+');
-        }
+    if(field.value.length == 0 && testValue.includes(y)){
+        return false;
+        
     }
+    field.value += y;
+    
 }
 
 // Clear the Field
 let button = document.querySelector("button.clear").addEventListener('click', () => {
-    testValue = [];
     field.value = '';
 })
 
@@ -47,5 +27,5 @@ let button = document.querySelector("button.clear").addEventListener('click', ()
 // Show the result
 function result(){
     let result = eval(field.value);
-    field.value = result
+    field.value = result==undefined?'':result;
 }
